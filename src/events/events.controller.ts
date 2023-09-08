323effab4e99221e1e4fce53b9675e8d8b55ef37
Samia Tanveer <samiatanveer_intern@netixsol.com>
2023-09-08 16:19:14 +0500

    Update events.controller.ts

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,14 +1,12 @@
 // src/events/events.controller.ts
-import { Controller, Post, Body, Get, UseGuards, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { CreateEventDto } from './dto/create-event.dto';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
-  @UseGuards(AuthGuard())
   @Post('create')
   createEvent(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.createEvent(createEventDto);
